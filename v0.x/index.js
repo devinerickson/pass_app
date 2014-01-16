1352390af3c49415608d6cea1311cd20ac09fd09
Devin <devinerickson@knights.ucf.edu>
2014-01-16 09:48:19 -0500

    updated for bluetooth plugin

--- v0.x/index.js
+++ v0.x/index.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+// Get the Bluetooth plugin
+var bluetoothSerial = cordova.require('bluetoothSerial');
+
+// Get the current date
+function getDate() {
+	var today = new Date();
+	var currentDate = today.toDateString();
+	return currentDate;
+}
